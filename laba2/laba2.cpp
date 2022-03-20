80c631d6d4d0afc1a2aba745ed4232240d03711f
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-03-20 21:46:00 +0300

    Добавьте файлы проекта.

---
+++ laba2/laba2.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <mpi.h>
+#include <fstream>
+using namespace std;
+
+const int n = 15, m = 10;
+double A[n][m], B[m][n], v[n], d[n], C[n][n];
+
+void FillMatrix(double(&AA)[n][m], double(&BB)[m][n]) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++) {
+			AA[i][j] = rand() % 100 / 2.5;
+		}
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			BB[i][j] = rand() % 100 / 2.5;
+		}
+	}
+}
+void FillVector(double v1[n]) {
+	for (int j = 0; j < n; j++) {
+		v1[j] = rand() % 100 / 2.5;
+	}
+}
+
+void Matrix_Peremnoj() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			C[i][j] = 0;
+			for (int k = 0; k < m; k++) {
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+}
+void Matrix_Peremnoj_na_vector(double A1[n][m], double v1[n]) {
+	for (int i = 0; i < n; i++) {
+		d[i] = 0;
+		for (int j = 0; j < m; j++) {
+			d[i] += v1[i] * A1[i][j];
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+
+	FillMatrix(A, B);
+	FillVector(v);
+
+	ofstream File1("Matrix_1.txt");
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++) {
+			File1 << A[i][j] << " ";
+		}
+		File1 << endl;
+	}
+	File1.close();
+
+	ofstream File2("Matrix_2.txt");
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			File2 << B[i][j] << " ";
+		}
+		File2 << endl;
+	}
+	File2.close();
+
+	Matrix_Peremnoj();
+
+	ofstream File3("Matrix_Otvet.txt");
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			File3 << C[i][j] << " ";
+		}
+		File3 << endl;
+	}
+	File3.close();
+
+	ofstream File4("Vector_1.txt");
+	for (int i = 0; i < n; i++)
+	{
+		File4 << v[i] << endl;
+	}
+	File4.close();
+
+	Matrix_Peremnoj_na_vector(A, v);
+	ofstream File5("Vector_Otvet.txt");
+	for (int i = 0; i < n; i++)
+	{
+		File5 << d[i] << endl;
+	}
+	File5.close();
+
+	MPI_Finalize();
+}
